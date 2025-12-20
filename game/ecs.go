d9cf934d8e00574a0d1f40c1354a48be6dd908f6
Igor Karatayev <igor.cwer@gmail.com>
2025-12-20 10:15:48 +0300

    Added more test data

---
+++ game/ecs.go
@@ -0,0 +1,43 @@
+package game
+
+import (
+	ecs "github.com/igadmg/goecs"
+	"github.com/igadmg/gogen-test/gfx"
+)
+
+type WorldModelComponent struct {
+	_ ecs.MetaTag `ecs:"component"`
+
+	Date string `ecs:"a, dto"`
+}
+
+type WorldEntity struct {
+	_  ecs.MetaTag `ecs:"archetype"`
+	Id ecs.Id
+
+	Model *WorldModelComponent
+}
+
+type ScreenViewComponent struct {
+	_ ecs.MetaTag `ecs:"component"`
+
+	background ecs.Ref[gfx.DrawEntity] `ecs:"a"` // background is transient ref here, because DrawEntity s transient, should be created automaticaly on create transient step
+}
+
+type ScreenModelComponent struct {
+	_ ecs.MetaTag `ecs:"component"`
+
+	worldMap ecs.Ref[WorldEntity] `ecs:"a, reference"` // reference components should not be created by default, but also not recreated as transient refs
+}
+
+type ScreenEntity struct {
+	_  ecs.MetaTag `ecs:"archetype"`
+	Id ecs.Id
+
+	View  *ScreenViewComponent
+	Model *ScreenModelComponent
+}
+
+func (s ScreenEntity) DrawBackground() {
+
+}
--- gfx/ecs.go
+++ gfx/ecs.go
@@ -1,14 +1,21 @@
 package gfx
 
-import ecs "github.com/igadmg/goecs"
+import (
+	"github.com/igadmg/gamemath/rect2"
+	ecs "github.com/igadmg/goecs"
+)
+
+type DrawCallFn func(source rect2.Float32)
 
 type DrawComponent struct {
 	_ ecs.MetaTag `ecs:"component"`
+
+	DrawCall DrawCallFn
 }
 
 type DrawEntity struct {
-	_  ecs.MetaTag `ecs:"archetype"`
+	_  ecs.MetaTag `ecs:"archetype: { transient }"`
 	Id ecs.Id
 
-	Component *DrawComponent
+	Component *DrawComponent `gog:"new: drawCall"`
 }
