7be85d0b141108192e2af4251efcb3e15f5f6c3c
Igor Karatayev <igor.cwer@gmail.com>
2026-01-05 02:59:23 +0300

    query test

--- ui/ecs.go
+++ ui/ecs.go
@@ -15,3 +15,47 @@ type LayoutComponent struct {
 	Context
 	Layout LayoutComponentI `gog:"new: 'layout'"`
 }
+
+type DragSourceQuery struct {
+	ecs.MetaTag `ecs:"query"`
+	ecs.Query
+
+	DragSource *DragSourceComponent
+}
+
+type DragSourceComponent struct {
+	ecs.MetaTag `ecs:"component"`
+
+	Target int
+}
+
+type DragSourceComponent2 struct {
+	ecs.MetaTag `ecs:"component"`
+	DragSourceComponent
+}
+
+type DragSourceComponent3 struct {
+	ecs.MetaTag `ecs:"component"`
+	DragSourceComponent2
+}
+
+type DragSourceEntity struct {
+	ecs.MetaTag `ecs:"archetype: { transient }"`
+	ecs.Archetype
+
+	DragSource *DragSourceComponent `ecs:"virtual"`
+}
+
+type DragSource2Entity struct {
+	ecs.MetaTag `ecs:"archetype: { transient }"`
+	DragSourceEntity
+
+	DragSource *DragSourceComponent2 `ecs:"virtual"`
+}
+
+type DragSource3Entity struct {
+	ecs.MetaTag `ecs:"archetype: { transient }"`
+	DragSource2Entity
+
+	DragSource *DragSourceComponent3 `ecs:"virtual"`
+}
